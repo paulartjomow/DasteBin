5c95fb96816cdf23958b032fa64fa0bca913752b
Paul Artjomow <paul-artjomow@hotmail.com>
2022-01-30 02:33:59 +0100

    Printing port on startup

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ This is free and open source software.
 
   // Start the server with the port number
   app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started");
+    console.log("Server started on port " + (process.env.PORT || 3000));
   });
 
   const getCode = async (cid) => {
