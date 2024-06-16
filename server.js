b36f6fdc22830b96a8b57d45cab5b4996469a746
Paul Artjomow <paul-artjomow@hotmail.com>
2024-06-16 18:38:39 +0200

    Replaced ipfs with helia, removed semicolons

--- server.js
+++ server.js
@@ -1,95 +1,104 @@
-const express = require("express");
-const IPFS = require("ipfs-core");
-const CID = require("cids");
-const fs = require("fs");
+import express from 'express'
+import { createHelia } from 'helia'
+import { unixfs } from '@helia/unixfs'
+import { CID } from 'multiformats/cid'
+import fs from 'fs'
+import path from 'path'
 
 async function main() {
-  const ipfs = await IPFS.create();
+  const helia = await createHelia()
+  const ufs = unixfs(helia)
+  const encoder = new TextEncoder()
 
-  const app = express();
+  const app = express()
 
-  app.set("view engine", "ejs");
-  app.use(express.static("public"));
-  app.use(express.urlencoded({ extended: true }));
-  app.use(express.json());
+  app.set("view engine", "ejs")
+  app.use(express.static("public"))
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
 
   app.get("/", (req, res) => {
-    res.redirect("/new");
-  });
+    res.redirect("/new")
+  })
 
   app.get("/new", (req, res) => {
-    res.render("new");
-  });
+    res.render("new")
+  })
 
   app.post("/new", async (req, res) => {
-    const code = req.body.code;
+    const code = req.body.code
 
-    //Check if code is valid
+    // Check if code is valid
     if (code.length < 1) {
-      res.json({ error: "Code is empty" });
-      return;
+      res.json({ error: "Code is empty" })
+      return
     }
 
-    const hash = await ipfs.add(code);
+    const bytes = encoder.encode(code)
+    
+    const cid = await ufs.addBytes(bytes)
 
-    res.json({ hash: hash.path });
+    res.json({ hash: cid.toString() })
   });
 
   app.post("/save", async (req, res) => {
     const code = req.body.code;
-    const hash = await ipfs.add(code);
+    const bytes = encoder.encode(code)
+    
+    const cid = await ufs.addBytes(bytes)
 
-    res.redirect("/" + hash.path);
+    res.redirect("/" + cid.toString())
   });
 
-  app.get("/about.md", async (req, res) => {
-    const code = fs.readFileSync("about.md", "utf8");
+  app.get("/about.md", (req, res) => {
+    const code = fs.readFileSync(path.resolve("about.md"), "utf8")
 
-    res.render("code-display", { code, language: "markdown" });
+    res.render("code-display", { code, language: "markdown" })
   });
 
   app.get("/:cid", async (req, res) => {
     let cid = req.params.cid;
     let code = await getCode(cid);
 
-    res.render("code-display", { code, cid });
+    res.render("code-display", { code, cid })
   });
 
   app.get("/raw/:cid", async (req, res) => {
-    let code = await getCode(req.params.cid);
+    let code = await getCode(req.params.cid)
 
     res.send(code);
   });
 
   app.get("/duplicate/:cid", async (req, res) => {
     let cid = req.params.cid;
-    let code = await getCode(cid);
+    let code = await getCode(cid)
 
-    res.render("new", { code });
+    res.render("new", { code })
   });
 
   // Start the server with the port number
-  app.listen(process.env.PORT || 3000, () => {
-    console.log("Server started on port " + (process.env.PORT || 3000));
-  });
+  app.listen(process.env.PORT || 3000, process.env.IP || '127.0.0.1', () => {
+    console.log("Server started on port " + (process.env.PORT || 3000))
+  })
 
   const getCode = async (cid) => {
     try {
-      cid = new CID(cid);
+      cid = CID.parse(cid)
+      const decoder = new TextDecoder()
 
-      let data = await ipfs.cat(cid, { timeout: 10000 });
+      let code = ""
 
-      let code = "";
-
-      for await (const chunk of data) {
-        code += chunk.toString("utf8");
+      for await(const chunk of ufs.cat(cid)) {
+        code += decoder.decode(chunk, {
+          stream: true
+        })
       }
 
-      return code;
+      return code
     } catch (err) {
-      return fs.readFileSync("invalid.md", "utf8");
+      return fs.readFileSync(path.resolve("invalid.md"), "utf8")
     }
-  };
+  }
 }
 
-main();
+main()
